a07586ac23363f77f8bce732688e751fbe9f41b1
大山祐司 <yuji@ooyamayuuji-no-MacBook-Air.local>
2021-01-06 19:05:08 +0900

    1/6 作業

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+
 import './App.css';
+
 import Person from './Person/Person';
+
+
 
 //class based
 class App extends Component {
@@ -46,13 +50,6 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
 
     let persons = null;
 
@@ -69,17 +66,26 @@ class App extends Component {
           })}
         </div>
       )
+
+    }
+
+    let classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); //classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold'); // classes = ['red', 'bold']
     }
 
     return (
+      
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button 
-        style={style}
-        onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {persons}
-      </div>
+      <h1>Hi, I'm a React App</h1>
+      <p className={classes.join(' ')}>This is really working!</p>
+        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
+      {persons}
+    </div>
+      
     );
   }  
 }
